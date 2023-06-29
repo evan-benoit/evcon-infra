cec06577149b9bdb89943eac540539b66c009b38
evan-benoit <evan.benoit@expel.io>
2023-06-29 13:10:39 -0400

    index now gets display names too

--- src/populateLeagueSeason/buildIndex.py
+++ src/populateLeagueSeason/buildIndex.py
@@ -11,7 +11,12 @@ def getCountries():
     countryCollection = db.collection("countries").stream()
     for country in countryCollection:
         print(country.id)
+        country_ref = db.collection("countries").document(country.id)
+        country_dict = country_ref.get().to_dict()
+
         index[country.id] = {}
+        index[country.id]["display"] = country_dict["display"]
+        index[country.id]["leagues"] = {}
 
         getLeagues(country.id)
 
@@ -20,7 +25,13 @@ def getLeagues(countryCode):
     leagueCollection = db.collection("countries/" + countryCode + "/leagues").stream()
     for league in leagueCollection:
         print("  " + league.id)
-        index[countryCode][league.id] = []
+
+        league_ref = db.collection("countries/" + countryCode + "/leagues").document(league.id)
+        league_dict = league_ref.get().to_dict()
+
+        index[countryCode]["leagues"][league.id] = {}
+        index[countryCode]["leagues"][league.id]["display"] = league_dict["display"]
+        index[countryCode]["leagues"][league.id]["seasons"] = []
 
         getSeasons(countryCode, league.id)
     
@@ -29,7 +40,7 @@ def getSeasons(countryCode, leagueID):
     seasonCollection = db.collection("countries/" + countryCode + "/leagues/" + leagueID + "/seasons").stream()
     for season in seasonCollection:
         print("    " + season.id)
-        index[countryCode][leagueID].append(season.id)
+        index[countryCode]["leagues"][leagueID]["seasons"].append(season.id)
 
     
 
