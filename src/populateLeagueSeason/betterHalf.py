b702e0dcf9b9be93953b7063a9656a94efa4f69a
evan-benoit <evan.benoit@expel.io>
2023-08-08 14:29:41 -0400

    cleaned up code, now runs, time to test

--- src/populateLeagueSeason/betterHalf.py
+++ src/populateLeagueSeason/betterHalf.py
@@ -6,10 +6,10 @@ from google.cloud import secretmanager
 from datetime import datetime
 from colors import (teamBorderColor, teamBackgroundColor, defaultColors)
 from buildIndex import (buildIndex)
+import datetime
 from datetime import date
-from datetime import time
-from datetime import datetime
 from datetime import timedelta
+
 
 
 db = firestore.Client(project='evcon-app')
@@ -26,35 +26,33 @@ headers = {
     'X-RapidAPI-Host': "api-football-v1.p.rapidapi.com"
     }
 
-todaysDate = datetime.today().strftime('%Y-%m-%d')
-# todaysDate = '2023-03-04'
-todaysDateTime = datetime.today().strftime('%Y-%m-%dT:%h:%m:%s')
 
-
-def getGamesForDate(request):
+def getGamesForRequest(request):
     request_json = request.get_json()
+    getGamesForDateRange(request_json)
+
+
+def getGamesForDateRange(request_json):
     # get the countryCode from the request
     countryCode = request_json['countryCode']
     # get the leagueID from the request
     leagueID = request_json['leagueID']
     # get the seasonID from the request
     season = request_json['seasonID']
-    # get the startDate from the request
-    startDate = request_json['startDate']
+    # get the startDate from the request, and convert it into a date object
+    startDate = datetime.datetime.strptime(request_json['startDate'], '%Y-%m-%d').date()    
     # get the endDate from the request
-    endDate = request_json['endDate']
+    endDate = datetime.datetime.strptime(request_json['endDate'], '%Y-%m-%d').date()    
     # get the timezone from the request
     timezone = request_json['timezone']
-
 
     # create an empty array for the games
     games = []
 
-    # loop between startDate and endDate
     indexDate = startDate
     while indexDate <= endDate:
         # get the games for the date, and append them to games
-        games = games + getGamesForDate(countryCode, leagueID, season, indexDate, timezone)
+        games.append(getGamesForDate(countryCode, leagueID, season, str(indexDate), timezone))
         # increment the date by 1 day
         indexDate = indexDate + timedelta(days=1) 
 
@@ -64,25 +62,25 @@ def getGamesForDate(request):
 
 def getGamesForDate(countryCode, leagueID, season, date, timezone):
 
-    # get today's date
-    todaysDate = datetime.today().strftime('%Y-%m-%d')
 
     # See if we have the games for that league, season, date in the database
     dateGames = db.collection("countries/" + countryCode + 
                               "/leagues/" + str(leagueID) + 
                               "/seasons/" + str(season) + 
-                              "/games/" + str(date)).get() 
+                              "/games").document(str(date)).get() 
 
     # if we have the games in the database, return them unless we're querying for today
-    if dateGames.exists and date != todaysDate:
-        return dateGames
+    if dateGames.exists:
+        print ("found " + date)
+
+        return dateGames.to_dict()
     else:
 
         # get the games for the date
         conn.request("GET", "/v3/fixtures?league=" + str(leagueID) + 
                                         "&season=" + str(season) + 
-                                        "&from=" + date 
-                                        "&to=" + date
+                                        "&from=" + date +
+                                        "&to=" + date +
                                         "&timezone" + timezone
                                         , headers=headers)
 
@@ -92,11 +90,11 @@ def getGamesForDate(countryCode, leagueID, season, date, timezone):
         # convert the response to a json object
         data = json.loads(data.decode("utf-8"))
         # get the fixtures from the response
-        fixtures = data['api']['fixtures']
+        response = data['response']
         # create an empty array for the games
         games = []
         # loop through the fixtures
-        for fixture in fixtures:
+        for fixture in response:
             homeTeam = fixture["teams"]["home"]["name"]
             awayTeam = fixture["teams"]["away"]["name"]
 
@@ -121,17 +119,27 @@ def getGamesForDate(countryCode, leagueID, season, date, timezone):
 
             games.append(game)
 
+        print ("storing " + games)
+
         # store the games in the firebase DB
         db.collection("countries/" + countryCode + 
                               "/leagues/" + str(leagueID) + 
                               "/seasons/" + str(season) + 
-                              "/games/" + str(date)).document().set(games)
+                              "/games").document(str(date)).set(games)
         
-        return games
+        return data
         
             
 
-                            
+
+request_json = {'countryCode': 'US', 
+                'leagueID': 254, 
+                'seasonID': 2023, 
+                'startDate': '2023-07-01', 
+                'endDate': '2023-07-15', 
+                'timezone': 'America/New_York'}
 
 
 
+
+getGamesForDateRange(request_json)
