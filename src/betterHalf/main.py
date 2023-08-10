ecfa1b4dd1ebe7ecb2369c68f4d3b234fe342afc
evan-benoit <evan.benoit@expel.io>
2023-08-10 09:42:46 -0400

    modified so that it always gets two seasons

--- src/betterHalf/main.py
+++ src/betterHalf/main.py
@@ -46,8 +46,6 @@ def getGamesForRequest(request):
     countryCode = request.args.get('countryCode')
     # get the leagueID from the request
     leagueID = request.args.get('leagueID')
-    # get the seasonID from the request
-    season = request.args.get('seasonID')
     # get the startDate from the request, and convert it into a date object
     startDate = datetime.datetime.strptime(request.args.get('startDate'), '%Y-%m-%d').date()
     # get the endDate from the request
@@ -55,7 +53,7 @@ def getGamesForRequest(request):
     # get the timezone from the request
     timezone = request.args.get('timezone')
 
-    games = getGamesForDateRange(countryCode, leagueID, season, startDate, endDate, timezone)
+    games = getGamesForDateRange(countryCode, leagueID, startDate, endDate, timezone)
 
     # return a flask response with the games
     return (json.dumps(games), 200, {'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*"})
@@ -63,7 +61,7 @@ def getGamesForRequest(request):
 
 
 
-def getGamesForDateRange(countryCode, leagueID, season, startDate, endDate, timezone):
+def getGamesForDateRange(countryCode, leagueID, startDate, endDate, timezone):
 
     # create an empty array for the games
     games = []
@@ -71,7 +69,7 @@ def getGamesForDateRange(countryCode, leagueID, season, startDate, endDate, time
     indexDate = startDate
     while indexDate <= endDate:
         # get the games for the date, and append them to games
-        games += getGamesForDate(countryCode, leagueID, season, str(indexDate), timezone)
+        games += getGamesForDate(countryCode, leagueID, str(indexDate), timezone)
         # increment the date by 1 day
         indexDate = indexDate + timedelta(days=1) 
 
@@ -79,13 +77,12 @@ def getGamesForDateRange(countryCode, leagueID, season, startDate, endDate, time
     return games 
 
 
-def getGamesForDate(countryCode, leagueID, season, date, timezone):
+def getGamesForDate(countryCode, leagueID, date, timezone):
 
 
-    # See if we have the games for that league, season, date in the database
+    # See if we have the games for that league, date in the database
     dateGames = db.collection("countries/" + countryCode + 
                               "/leagues/" + str(leagueID) + 
-                              "/seasons/" + str(season) + 
                               "/games").document(str(date)).get() 
 
     # if we have the games in the database, return them unless we're querying for today
@@ -97,52 +94,60 @@ def getGamesForDate(countryCode, leagueID, season, date, timezone):
         return gamesArray
     else:
 
-        requestString = "/v3/fixtures?league=" + str(leagueID) + "&season=" + str(season) + "&date=" + date + "&timezone=" + timezone
+        # Loop on the date's year and the previous year, since the season may span two years
+        year = date.split('-')[0]
+        previousYear = str(int(year) - 1)
+        years = [year, previousYear]
 
-        # get the games for the date
-        conn.request("GET", requestString, headers=headers)
-
-        print ("request: " + requestString)
-
-        
-        res = conn.getresponse()
-        data = res.read()
-        # convert the response to a json object
-        data = json.loads(data.decode("utf-8"))
-        # get the fixtures from the response
-        response = data['response']
         # create an empty array for the games
         games = []
-        # loop through the fixtures
-        for fixture in response:
-            fixtureID = fixture["fixture"]["id"]
-            homeTeam = fixture["teams"]["home"]["name"]
-            awayTeam = fixture["teams"]["away"]["name"]
 
-            print (homeTeam + " vs " + awayTeam)
+        for year in years:
 
-            #strip the trailing W that FOOTBALL-API adds to NWSL teams.  Fuck the patriarchy!
-            homeTeam = homeTeam.rstrip(' W')
-            awayTeam = awayTeam.rstrip(' W')
+            requestString = "/v3/fixtures?league=" + str(leagueID) + "&season=" + year + "&date=" + date + "&timezone=" + timezone
 
-            homeHalftimeScore = int(fixture["score"]["halftime"]["home"])
-            awayHalftimeScore = int(fixture["score"]["halftime"]["away"])
+            # get the games for the date
+            conn.request("GET", requestString, headers=headers)
 
-            homeFinalScore = int(fixture["score"]["fulltime"]["home"])
-            awayFinalScore = int(fixture["score"]["fulltime"]["away"])
+            print ("request: " + requestString)
 
-            # put these variables in a dictionary
-            game = {'date' : date,
-                    'fixtureID': fixtureID,
-                    'homeTeam': homeTeam,
-                    'awayTeam': awayTeam,
-                    'homeHalftimeScore': homeHalftimeScore,
-                    'awayHalftimeScore': awayHalftimeScore,
-                    'homeFinalScore': homeFinalScore,
-                    'awayFinalScore': awayFinalScore,                
-            }
+            res = conn.getresponse()
+            data = res.read()
+            # convert the response to a json object
+            data = json.loads(data.decode("utf-8"))
+            # get the fixtures from the response
+            response = data['response']
 
-            games.append(game)
+            # loop through the fixtures
+            for fixture in response:
+                fixtureID = fixture["fixture"]["id"]
+                homeTeam = fixture["teams"]["home"]["name"]
+                awayTeam = fixture["teams"]["away"]["name"]
+
+                print (homeTeam + " vs " + awayTeam)
+
+                #strip the trailing W that FOOTBALL-API adds to NWSL teams.  Fuck the patriarchy!
+                homeTeam = homeTeam.rstrip(' W')
+                awayTeam = awayTeam.rstrip(' W')
+
+                homeHalftimeScore = int(fixture["score"]["halftime"]["home"])
+                awayHalftimeScore = int(fixture["score"]["halftime"]["away"])
+
+                homeFinalScore = int(fixture["score"]["fulltime"]["home"])
+                awayFinalScore = int(fixture["score"]["fulltime"]["away"])
+
+                # put these variables in a dictionary
+                game = {'date' : date,
+                        'fixtureID': fixtureID,
+                        'homeTeam': homeTeam,
+                        'awayTeam': awayTeam,
+                        'homeHalftimeScore': homeHalftimeScore,
+                        'awayHalftimeScore': awayHalftimeScore,
+                        'homeFinalScore': homeFinalScore,
+                        'awayFinalScore': awayFinalScore,                
+                }
+
+                games.append(game)
 
 
         data = {}
@@ -155,7 +160,6 @@ def getGamesForDate(countryCode, leagueID, season, date, timezone):
             # store the games in the firebase DB
             db.collection("countries/" + countryCode + 
                                 "/leagues/" + str(leagueID) + 
-                                "/seasons/" + str(season) + 
                                 "/games").document(str(date)).set(data)
             
         return games
@@ -166,9 +170,8 @@ def getGamesForDate(countryCode, leagueID, season, date, timezone):
 
 # games = getGamesForDateRange('us', 
 #                              254, 
-#                              2023, 
-#                              datetime.datetime.strptime('2023-08-01', '%Y-%m-%d').date(), 
-#                              datetime.datetime.strptime('2023-08-08', '%Y-%m-%d').date(), 
+#                              datetime.datetime.strptime('2023-07-01', '%Y-%m-%d').date(), 
+#                              datetime.datetime.strptime('2023-07-08', '%Y-%m-%d').date(), 
 #                              'America/New_York')
 
 # print (games)
