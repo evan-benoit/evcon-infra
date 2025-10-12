01befb9de72c5e32c990de8de6720e75ebcc1e52
Evan Benoit <evan.m.benoit@gmail.com>
2025-10-12 10:43:30 -0400

    pinned my version numbers

--- tf/versions.tf
+++ tf/versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.6.0"
+      version = "~> 7.6"
     }
   }
 
-  required_version = ">= 0.14"
+  required_version = "~> 1.13.0"
 }
 
